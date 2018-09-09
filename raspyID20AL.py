61801751c18703782523c81388d5c7a43f071307
dazzacurdan <pasterispaolo@gmail.com>
2018-09-09 13:01:36 +0200

    initial commit

---
+++ raspyID20AL.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#
+# https://www.raspberrypi.org/forums/viewtopic.php?uid=95140&f=48&t=31552
+# http://bradsrpi.blogspot.com/2013/01/rfid-with-innovations-id12-rfid-reader.html
+# https://www.raspberrypi.org/forums/viewtopic.php?t=76983
+#
+# Import required libraries
